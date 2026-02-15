e79023942e1e1db9b98ba4f991d34feddfe6d95b
Fernando Siller <fersill53@gmail.com>
2026-02-15 16:29:45 -0600

    finishing up

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { provideNativeDateAdapter } from '@angular/material/core';
@@ -8,7 +8,7 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideAnimations(),
     provideNativeDateAdapter(),
   ],
